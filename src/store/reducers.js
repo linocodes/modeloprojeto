e95a70cb150c1315f4fa7bb724603d014fd3e0d3
Marcelo Lino <marcelo.lino.santos@gmail.com>
2018-10-19 18:40:55 -0300

    ajustes

---
+++ src/store/reducers.js
@@ -0,0 +1,29 @@
+import { combineReducers } from 'redux';
+import { routerReducer as router } from 'react-router-redux';
+import { reducer as notifications } from 'react-notification-system-redux';
+
+import layout from 'layouts/DefaultLayout/modules/layout';
+
+export const makeRootReducer = (asyncReducers) => {
+  return combineReducers({
+    // Add sync reducers here
+    layout,
+    router,
+    notifications,
+    ...asyncReducers
+  })
+}
+
+export const injectReducer = (store, { key, reducer }) => {
+  store.asyncReducers[key] = reducer
+  store.replaceReducer(makeRootReducer(store.asyncReducers))
+}
+
+export const getDefaultInitialState = () => {
+  const initialState = {}
+  // add to initial states as routes are added
+
+  return initialState
+}
+
+export default makeRootReducer
